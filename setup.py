4e55299ceb03cdf313144c3551d5c901cce6fc9d
elishamod <elishamod@gmail.com>
2024-08-02 12:37:23 +0300

    will it work now?

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
+from os.path import join
+source_dir = 'src'
 
 setup(
     name="karnaf_tools",
-    version="0.0.3",
-    packages=find_packages(),
+    version="0.0.4",
+    packages=[join(source_dir, pkg) for pkg in find_packages(source_dir)],
     install_requires=[
         # List your dependencies here
         "matplotlib",
