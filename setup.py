e113d80f28fcca9075816c6722a7da6f97e0a94e
elishamod <elishamod@gmail.com>
2024-08-02 10:22:44 +0300

    changed version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="my_package",
-    version="0.1",
+    version="0.2",
     # packages=find_packages(),
     packages=['src\karnaf_tools'],
     install_requires=[
