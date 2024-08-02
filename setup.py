65f256171adff9944e3304091e96e0794afb9e17
elishamod <elishamod@gmail.com>
2024-08-02 10:15:34 +0300

    fixed setup

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="my_package",
     version="0.1",
-    packages=find_packages(),
+    # packages=find_packages(),
+    packages=['src\karnaf_tools'],
     install_requires=[
         # List your dependencies here
         "matplotlib",
