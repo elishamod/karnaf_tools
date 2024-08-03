5302ea12eb37fda55c9a44ea1602be1ccd57c8ee
elishamod <elishamod@gmail.com>
2024-08-03 09:48:36 +0300

    updated to version 0.0.6

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="karnaf_tools",
-    version="0.0.5",
+    version="0.0.6",
     packages=find_packages(),
     install_requires=[
         # List your dependencies here
         "matplotlib",
-        "argparse"
+        "argparse",
+        "numpy"
     ],
 )
